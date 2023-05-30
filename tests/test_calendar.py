aa06517341370f0c4c83212a0f27a6dfcf3c3293
Alex Raichev <e@mail.net>
2023-05-30 14:01:10 +1200

    Removed star imports from some tests

--- tests/test_calendar.py
+++ tests/test_calendar.py
@@ -1,15 +1,18 @@
-from .context import gtfs_kit, sample, cairns
-from gtfs_kit import *
+import functools as ft
+
+from .context import cairns
+from .context.gtfs_kit import calendar as gkc
+from .context.gtfs_kit import helpers as hp
 
 
 def test_get_dates():
     feed = cairns.copy()
     for as_date_obj in [True, False]:
-        dates = get_dates(feed, as_date_obj=as_date_obj)
+        dates = gkc.get_dates(feed, as_date_obj=as_date_obj)
         d1 = "20140526"
         d2 = "20141228"
         if as_date_obj:
-            d1, d2 = map(datestr_to_date, [d1, d2])
+            d1, d2 = ft.map(hp.datestr_to_date, [d1, d2])
             assert len(dates) == (d2 - d1).days + 1
         assert dates[0] == d1
         assert dates[-1] == d2
@@ -20,18 +23,18 @@ def test_get_dates():
         feed1.calendar_dates = c[:0]
         c = feed1.calendar
         feed1.calendar = c[:0]
-        dates = get_dates(feed1, as_date_obj=as_date_obj)
+        dates = gkc.get_dates(feed1, as_date_obj=as_date_obj)
         assert not dates
 
 
 def test_get_week():
     feed = cairns.copy()
     for as_date_obj in [True, False]:
-        dates = get_week(feed, 1, as_date_obj=as_date_obj)
+        dates = gkc.get_week(feed, 1, as_date_obj=as_date_obj)
         d1 = "20140526"
         d2 = "20140601"
         if as_date_obj:
-            d1, d2 = map(datestr_to_date, [d1, d2])
+            d1, d2 = ft.map(hp.datestr_to_date, [d1, d2])
             assert len(dates) == (d2 - d1).days + 1
         assert dates[0] == d1
         assert dates[-1] == d2
@@ -40,11 +43,11 @@ def test_get_week():
 def test_get_first_week():
     feed = cairns.copy()
     for as_date_obj in [True, False]:
-        dates = get_first_week(feed, as_date_obj=as_date_obj)
+        dates = gkc.get_first_week(feed, as_date_obj=as_date_obj)
         d1 = "20140526"
         d2 = "20140601"
         if as_date_obj:
-            d1, d2 = map(datestr_to_date, [d1, d2])
+            d1, d2 = ft.map(hp.datestr_to_date, [d1, d2])
             assert len(dates) == (d2 - d1).days + 1
         assert dates[0] == d1
         assert dates[-1] == d2
@@ -53,6 +56,6 @@ def test_get_first_week():
 def test_subset_dates():
     feed = cairns.copy()
     dates = feed.get_dates()
-    assert subset_dates(feed, [dates[0]]) == [dates[0]]
-    assert subset_dates(feed, ["9999"]) == []
-    assert subset_dates(feed, dates + ["999"]) == dates
+    assert gkc.subset_dates(feed, [dates[0]]) == [dates[0]]
+    assert gkc.subset_dates(feed, ["9999"]) == []
+    assert gkc.subset_dates(feed, dates + ["999"]) == dates
